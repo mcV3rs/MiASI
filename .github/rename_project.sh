97a3280b5aecbd8e3185e9f38d7aeff3fe45b2a4
mcV3rs <mcV3rs@users.noreply.github.com>
2024-11-11 19:15:00 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="mcV3rs"
+original_name="miasi"
+original_urlname="MiASI"
+original_description="Awesome miasi created by mcV3rs"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv miasi $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
